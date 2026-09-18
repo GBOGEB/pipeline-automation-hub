fcd7b2bac252e48fcf3f90bead7912a9eee9d428
GBOGEB <gerkotze.bonthuys@sckcen.be>
2026-09-18 13:50:16 +0200

    W280: regress KPI fail-open review findings

--- mission-control/grand-missions/tests/test_r3_dmaic_kpis.py
+++ mission-control/grand-missions/tests/test_r3_dmaic_kpis.py
@@ -12,6 +12,7 @@ class TestR3DMAICKPI(unittest.TestCase):
 
     def test_exact_metrics(self):
         got=kpi.validate(self.doc)
+        self.assertEqual(set(got),set(self.doc["expected_kpis"]))
         self.assertEqual(got["pr_count"],8)
         self.assertEqual(got["mean_merge_lead_seconds"],210)
         self.assertEqual(got["median_merge_lead_seconds"],169)
@@ -25,6 +26,14 @@ class TestR3DMAICKPI(unittest.TestCase):
 
     def test_metric_drift_rejected(self):
         d=copy.deepcopy(self.doc); d["population"]["prs"][0]["merge_lead_seconds"]=204
+        with self.assertRaises(SystemExit): kpi.validate(d)
+
+    def test_missing_expected_kpi_rejected(self):
+        d=copy.deepcopy(self.doc); d["expected_kpis"].pop("current_successor_binding_delta")
+        with self.assertRaises(SystemExit): kpi.validate(d)
+
+    def test_non_boolean_closed_rejected(self):
+        d=copy.deepcopy(self.doc); d["observed_defect_classes"][0]["closed"]="false"
         with self.assertRaises(SystemExit): kpi.validate(d)
 
 if __name__=="__main__":
